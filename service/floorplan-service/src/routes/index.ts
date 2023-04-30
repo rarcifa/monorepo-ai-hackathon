fce71977ecd07bda18ed613b65139e793af7cae8
rarcifa <ricardo.arcifa@cronoslabs.org>
2023-04-30 19:49:36 +0200

    refactoring

--- service/floorplan-service/src/routes/index.ts
+++ service/floorplan-service/src/routes/index.ts
@@ -1,5 +1,4 @@
 import healthRoutes from '@routes/health-routes';
-import logRoutes from '@routes/log-routes';
 import floorplanRoute from '@src/routes/floorplan-routes';
 import * as express from 'express';
 
@@ -12,5 +11,4 @@ export const register = (app: express.Application): void => {
   app.get('/');
   app.use('(/api)?/v1/floorplan-service', floorplanRoute);
   app.use('/healthcheck', healthRoutes);
-  app.use('/sendlog', logRoutes);
 };
