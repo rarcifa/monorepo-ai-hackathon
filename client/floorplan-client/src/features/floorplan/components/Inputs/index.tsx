bdbd9bb5fd34eed244497e868bda341406d8eccf
rarcifa <ricardo.arcifa@cronoslabs.org>
2023-04-30 16:06:27 +0200

    added a few states to generate the iamge and call the service API

--- client/floorplan-client/src/features/floorplan/components/Inputs/index.tsx
+++ client/floorplan-client/src/features/floorplan/components/Inputs/index.tsx
@@ -1,60 +1,125 @@
-import { useAppSelector, useAppDispatch } from 'app/hooks';
+import { useAppDispatch } from 'app/hooks';
 import { generateFloorPlansAction } from 'features/floorplan/actions/generateFloorPlansAction';
-import { IFloorplanSlice } from 'features/floorplan/floorplanSlice';
-import { IResult } from 'features/types/floorplan';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import styles from './index.module.scss';
+import { Button } from 'app/components/Button';
 
 export function Inputs(): JSX.Element {
-  const data: IFloorplanSlice = useAppSelector((state) => state.floorplan);
-  const generatedFloorplan: IResult | undefined = data.results;
-
-  const [totalFloor] = useState<number[]>([10, 10]);
-  const [roomOneFloor] = useState<number[]>([10, 10]);
-  const [roomTwoFloor] = useState<number[]>([10, 10]);
-  const [roomThreeFloor] = useState<number[]>([10, 10]);
-
-  // check results in console
-  console.log(generatedFloorplan);
+  const [total_area_input_one, setTotal_area_input_one] = useState<number>(0);
+  const [total_area_input_two, setTotal_area_input_two] = useState<number>(0);
+  const [room_one_input_one, setRoom_one_input_one] = useState<number>(0);
+  const [room_one_input_two, setRoom_one_input_two] = useState<number>(0);
+  const [room_two_input_one, setRoom_two_input_one] = useState<number>(0);
+  const [room_two_input_two, setRoom_two_input_two] = useState<number>(0);
+  const [room_three_input_one, setRoom_three_input_one] = useState<number>(0);
+  const [room_three_input_two, setRoom_three_input_two] = useState<number>(0);
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
+  const generateOnClick = useCallback(() => {
     dispatch(
       generateFloorPlansAction({
-        totalFloor,
-        roomOneFloor,
-        roomTwoFloor,
-        roomThreeFloor,
+        total_area_input_one,
+        total_area_input_two,
+        room_one_input_one,
+        room_one_input_two,
+        room_two_input_one,
+        room_two_input_two,
+        room_three_input_one,
+        room_three_input_two,
       })
     );
-  }, [totalFloor, roomOneFloor, roomTwoFloor, roomThreeFloor, dispatch]);
+  }, [
+    total_area_input_one,
+    total_area_input_two,
+    room_one_input_one,
+    room_one_input_two,
+    room_two_input_one,
+    room_two_input_two,
+    room_three_input_one,
+    room_three_input_two,
+    ,
+    dispatch,
+  ]);
 
   return (
     <div className={styles.inputsContainer}>
       <div className={styles.inputRow}>
         <span className={styles.inputTitle}>Total Floor:</span>
-        <input className={styles.input} />
+        <input
+          className={styles.input}
+          onChange={(e: any) => setTotal_area_input_one(e.target.value)}
+        />
         <span className={styles.divider}>x</span>
-        <input className={styles.input} /> m<sup>2</sup>
+        <input
+          className={styles.input}
+          onChange={(e: any) => setTotal_area_input_two(e.target.value)}
+        />
+        m<sup>2</sup>
       </div>
       <div className={styles.inputRow}>
         <span className={styles.inputTitle}> Room One:</span>
-        <input className={styles.input} />
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_one_input_one(e.target.value)}
+        />
         <span className={styles.divider}>x</span>
-        <input className={styles.input} /> m<sup>2</sup>
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_one_input_two(e.target.value)}
+        />
+        m<sup>2</sup>
       </div>
       <div className={styles.inputRow}>
         <span className={styles.inputTitle}> Roome Two:</span>
-        <input className={styles.input} />
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_two_input_one(e.target.value)}
+        />
         <span className={styles.divider}>x</span>
-        <input className={styles.input} /> m<sup>2</sup>
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_two_input_two(e.target.value)}
+        />
+        m<sup>2</sup>
       </div>
       <div className={styles.inputRow}>
         <span className={styles.inputTitle}> Room Three:</span>
-        <input className={styles.input} />
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_three_input_one(e.target.value)}
+        />
         <span className={styles.divider}>x</span>
-        <input className={styles.input} /> m<sup>2</sup>
+        <input
+          className={styles.input}
+          onChange={(e: any) => setRoom_three_input_two(e.target.value)}
+        />
+        m<sup>2</sup>
+      </div>
+      <div className={styles.generate}>
+        <Button onClick={() => generateOnClick()}>Generate</Button>
+      </div>
+      <div className={styles.architectureGrid}>
+        <div style={{ marginTop: 25 }}>
+          <span className={styles.inputTitle}>
+            Total Floor is {total_area_input_one} x {total_area_input_two}
+          </span>
+        </div>
+        <div>
+          <span className={styles.inputTitle}>
+            Room One is {room_one_input_one} x {room_one_input_two}
+          </span>
+        </div>
+        <div>
+          <span className={styles.inputTitle}>
+            Room Two is {room_two_input_one} x {room_two_input_two}
+          </span>
+        </div>
+        <div>
+          <span className={styles.inputTitle}>
+            Room Three is {room_three_input_one} x {room_three_input_two}
+          </span>
+        </div>
       </div>
     </div>
   );
--- client/floorplan-client/src/features/floorplan/components/Output/index.tsx
+++ client/floorplan-client/src/features/floorplan/components/Output/index.tsx
@@ -1,8 +1,25 @@
+import { useAppSelector } from 'app/hooks';
+import { IFloorplanSlice } from 'features/floorplan/floorplanSlice';
+import { IResult } from 'features/types/floorplan';
 import styles from './index.module.scss';
 
 export function Output(): JSX.Element {
+  const data: IFloorplanSlice = useAppSelector((state) => state.floorplan);
+  const generatedFloorplan: IResult | undefined = data.results;
+
+  const css = {
+    backgroundImage: `url(${generatedFloorplan?.data})`,
+    backgroundSize: 'cover',
+    backgroundPosition: '0% 0%',
+    backgroundRepeat: 'initial',
+    backgroundAttachment: 'initial',
+    backgroundOrigin: 'initial',
+    backgroundClip: 'initial',
+    backgroundColor: 'initial',
+  };
+
   return (
-    <div className={styles.outputContainer}>
+    <div className={styles.outputContainer} style={css}>
       <div className={styles.output} />
     </div>
   );
